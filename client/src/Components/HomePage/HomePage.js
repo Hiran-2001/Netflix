b95ba742c4e1037527c86b98dbb652342a586db1
Hiran Raj <hirankvlr@gmail.com>
2023-01-26 23:07:57 +0530

    working on banner

--- client/src/Components/HomePage/HomePage.js
+++ client/src/Components/HomePage/HomePage.js
@@ -3,7 +3,6 @@ import NavBar from "../NavBar/NavBar";
 import "./HomePage.css";
 import axios from "axios";
 function HomePage() {
-
   function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
@@ -16,26 +15,32 @@ function HomePage() {
   const apiUrl =
     "https://api.themoviedb.org/3/trending/all/day?api_key=bc3ab4f2543c6cf7322eda5564ceacfa";
   const fetchApi = async (e) => {
-    
-
     const res = await axios.get(apiUrl);
-    console.log(res.data.results[e]);
+    // console.log(res.data.results[e]);
+    // console.log(res.data.results[e].title);
     setHomeBanner(res.data.results[e]);
-
   };
-  const fetchPoster=(poster)=>{
-     return `https://www.themoviedb.org/t/p/w220_and_h330_face/${poster}`
-  }
+  const fetchPoster = (poster) => {
+    return `https://www.themoviedb.org/t/p/w220_and_h330_face/${poster}`;
+  };
 
-  const fetchBanner=async()=>{
-    const res = await axios.get(`https://www.themoviedb.org/discover/tv?api_key=bc3ab4f2543c6cf7322eda5564ceacfa&with_network=213`)
-  }
+  
   return (
     <div>
       <NavBar />
-      <div className="home-page-main">
-        <img className="home-page-main-image" src={fetchPoster(homeBanner.poster_path)} alt="NO Image" />
-        {/* <h1>{homeBanner.title}</h1> */}
+      <div
+        className="home-page-main"
+        style={{
+          backgroundImage: `url("https://image.tmdb.org/t/p/original${homeBanner.backdrop_path}")`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
+        {/* <img className="home-page-main-image" src={fetchBanner(homeBanner.backdrop_path)} alt="NO Image" /> */}
+        <h1>{homeBanner.title || homeBanner.name}</h1>
+      </div>
+      <div className="popular">
+        <h1>Popular on netflix</h1>
       </div>
     </div>
   );
